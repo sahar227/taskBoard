450d770e1fe137a9c34f634fe9449e723bde3e03
Sahar <saharoffice@gmail.com>
2020-03-28 15:21:24 +0300

    Added cors option to allow sending auth token

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const users = require('../routes/users');
-const auth = require('../routes/auth');
-const task = require('../routes/task');
-const board = require('../routes/board');
-const error = require('../middleware/error');
-const cors = require('cors');
+const express = require("express");
+const users = require("../routes/users");
+const auth = require("../routes/auth");
+const task = require("../routes/task");
+const board = require("../routes/board");
+const error = require("../middleware/error");
+const cors = require("cors");
+
+const corsOptions = {
+  exposedHeaders: "x-auth-token"
+};
 
 module.exports = function(app) {
   app.use(express.json());
-  app.use(cors());
-  app.use('/api/users', users);
-  app.use('/api/auth', auth);
-  app.use('/api/task', task);
-  app.use('/api/board', board);
+  app.use(cors(corsOptions));
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use("/api/task", task);
+  app.use("/api/board", board);
   app.use(error);
-}
+};
