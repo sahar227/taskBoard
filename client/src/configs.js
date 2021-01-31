af1183b21b0173b5405ca5b36cdb7d4beb9ccb17
sahar227 <sahar22798@gmail.com>
2021-01-31 12:06:29 +0200

    Removed commented code

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,2 +1,3 @@
 export const baseURL = "http://localhost:3000/api";
+// export const baseURL = "https://task-board227.herokuapp.com/api";
 export const authToken = "taskBoardAuthToken";
--- server/index.js
+++ server/index.js
@@ -8,5 +8,4 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
 
-// const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
