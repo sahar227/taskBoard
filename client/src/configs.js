66f22d0c7b98cd639d088735f851b0ceae7dcb0a
sahar227 <sahar22798@gmail.com>
2021-01-31 14:55:04 +0200

    Handle server url using .env

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,3 +1,2 @@
-export const baseURL = "http://localhost:3000/api";
-// export const baseURL = "https://task-board227.herokuapp.com/api";
+export const baseURL = process.env.REACT_APP_BASE_SERVER_URL;
 export const authToken = "taskBoardAuthToken";
